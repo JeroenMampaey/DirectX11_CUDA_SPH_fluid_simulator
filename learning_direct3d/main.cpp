ed89654f5b952301fdc567e2507000ff3fa52371
jeroen <jr.mampaey@gmail.com>
2022-08-29 03:24:18 +0200

    learning direct3d...

--- learning_direct3d/main.cpp
+++ learning_direct3d/main.cpp
@@ -11,18 +11,34 @@
 #include <wrl/client.h>
 #include <memory>
 
+#include "my_exception.h"
+#include <sstream>
 
 class MainWindow : public BaseWindow<MainWindow>
 {
-private:
-    std::unique_ptr<Graphics> pGfx = nullptr;
+    private:
+        std::unique_ptr<Graphics> pGfx = nullptr;
 
-public:
-    void Render();
-    MainWindow(){}
-    PCWSTR  ClassName() const { return L"SPH Window Class"; }
-    LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+    public:
+        class Exception : public MyException{
+            public:
+                Exception(int line, const char* file, HRESULT hr) noexcept;
+                const char* what() const noexcept override;
+                const char* GetType() const noexcept override;
+                static std::string TranslateErrorCode(HRESULT hr) noexcept;
+                HRESULT GetErrorCode() const noexcept;
+                std::string GetErrorString() const noexcept;
+            private:
+                HRESULT hr;
+        };
+
+        void Render();
+        MainWindow(){};
+        PCWSTR  ClassName() const { return L"SPH Window Class"; };
+        LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
 };
+
+#define CHWND_EXCEPT(hr) MainWindow::Exception(__LINE__, __FILE__, hr)
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
@@ -81,4 +97,51 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 void MainWindow::Render(){
     pGfx->ClearBuffer(0.1, 0.5, 1.0);
     pGfx->EndFrame();
+}
+
+MainWindow::Exception::Exception(int line, const char* file, HRESULT hr) noexcept
+    :
+    MyException(line, file),
+    hr(hr)
+{}
+
+const char* MainWindow::Exception::what() const noexcept{
+    std::ostringstream oss;
+    oss << GetType() << std::endl
+        << "[Error Code]" << GetErrorCode() << std::endl
+        << "[Description]" << GetErrorString() << std::endl
+        << GetOriginString();
+    whatBuffer = oss.str();
+    return whatBuffer.c_str();
+}
+
+const char* MainWindow::Exception::GetType() const noexcept{
+    return "MainWindow::Exception";
+}
+
+std::string MainWindow::Exception::TranslateErrorCode(HRESULT hr) noexcept{
+    char* pMsgBuf = nullptr;
+    DWORD nMsgLen = FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        nullptr,
+        hr,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        reinterpret_cast<LPWSTR>(&pMsgBuf),
+        0,
+        nullptr
+    );
+    if(nMsgLen==0){
+        return "Unidentified error code";
+    }
+    std::string errorString = pMsgBuf;
+    LocalFree(pMsgBuf);
+    return errorString;
+}
+
+HRESULT MainWindow::Exception::GetErrorCode() const noexcept{
+    return hr;
+}
+
+std::string MainWindow::Exception::GetErrorString() const noexcept{
+    return TranslateErrorCode(hr);
 }
